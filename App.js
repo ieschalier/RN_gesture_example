70fb7b7c548c11b7a72d4fce987b6196a25b6097
Igor ESCHALIER <igor.eschalier@gmail.com>
2018-05-19 17:43:09 +0200

    Zoom event

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, Animated } from 'react-native'
+import { State, PinchGestureHandler } from 'react-native-gesture-handler'
 
 export default class App extends React.Component {
+  baseScale = new Animated.Value(1)
+  pinchScale = new Animated.Value(1)
+  scale = Animated.multiply(this.baseScale, this.pinchScale)
+  lastScale = 1
+
+  onPinchGestureEvent = Animated.event(
+    [{ nativeEvent: { scale: this.pinchScale } }],
+    { useNativeDriver: true },
+  )
+
+  handlePinchHandlerStateChange = ev => {
+    if (ev.nativeEvent.oldState === State.ACTIVE) {
+      this.lastScale *= ev.nativeEvent.scale
+      this.baseScale.setValue(this.lastScale)
+      this.pinchScale.setValue(1)
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <PinchGestureHandler
+        onGestureEvent={this.onPinchGestureEvent}
+        onHandlerStateChange={this.handlePinchHandlerStateChange}
+      >
+        <Animated.View
+          style={[styles.container, { transform: [{ scale: this.scale }] }]}
+        >
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+          <View style={{ width: 50, height: 50, backgroundColor: 'red' }} />
+        </Animated.View>
+      </PinchGestureHandler>
+    )
   }
 }
 
@@ -20,4 +47,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
